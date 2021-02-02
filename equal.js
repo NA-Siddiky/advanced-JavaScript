a060490aec9577cbd730a7412077c723ea24528e
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-03 02:09:39 +0600

    adding == amd ===

---
+++ equal.js
@@ -0,0 +1,59 @@
+const first = 2;
+const secund = "2";
+
+if (first == secund) {
+    console.log('true')
+}
+else {
+    console.log('false')
+}
+if (first === secund) {
+    console.log('true')
+}
+else {
+    console.log('false')
+}
+
+
+
+const first1 = 0;
+const secund1 = false; //false is a negative value and in if () it multiplying. /
+
+if (first1 == secund1) {
+    console.log('true')
+}
+else {
+    console.log('false')
+}
+
+const first2 = 0;
+const secund2 = true; //false is a negative value and in if () it multiplying. /
+
+if (first2 == secund2) {
+    console.log('true')
+}
+else {
+    console.log('false')
+}
+
+const first3 = 1;
+const secund3 = false; //false is a negative value and in if () it multiplying. /
+
+if (first3 == secund3) {
+    console.log('true')
+}
+else {
+    console.log('false')
+}
+
+
+
+const first4 = 1;
+const secund4 = true; //false is a negative value and in if () it multiplying. /
+
+if (first4 == secund4) {
+    console.log('true')
+}
+else {
+    console.log('false')
+}
