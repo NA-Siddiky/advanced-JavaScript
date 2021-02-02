ce5f85b46bc0e1e92c5817e043f178ed596c5a4e
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-03 01:50:19 +0600

    null vs undefined

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,2 +1,31 @@
-let pakhi;
+let pakhi;                   //undefine in this way 1 /
 console.log(pakhi);
+
+
+function add(num1, num2) {           //undefine in this way 2 /
+    console.log(num1 + num2);
+    return
+}
+const result = add(12, 23);
+console.log(result);
+
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const resultIs = add(22, 10);
+console.log(resultIs);
+
+const premik = { name: "Ali", phone: 00001 };       //undefine in this way 3 /
+console.log(premik.gf);
+
+let fun = undefined;        //undefine in this way 4 /
+console.log(fun);
+
+let ages = [2, 3, 4, 5];
+console.log(ages[1]);
+console.log(ages[10]);          //undefine in this way 4 /
+
+
+// null ...  null..//
