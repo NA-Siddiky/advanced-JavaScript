894b2a691aa110c61994cf4e35b4d239559d58bb
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-03 20:41:40 +0600

    adding break-continue function

---
+++ break-continue.js
@@ -0,0 +1,27 @@
+// const nums = [1, 2, 3, 4, 5, 6];
+// for (let i = 0; i < nums.length; i++) {
+//     const element = nums[i];
+//     // console.log(element);
+//     if (element > 3) {
+//         break
+//     }
+//     console.log(element);
+// }
+
+
+const nums = [1, -2, 3, -4, 5, 6];
+for (let i = 0; i < nums.length; i++) {             //problem , hot the first look run ?/
+    const element = nums[i];
+    // console.log(element);
+    if (element > 3) {
+        break
+    }
+}
+for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    // console.log(element);
+    if (element < 0) {
+        continue
+    }
+    console.log(element);
+}
