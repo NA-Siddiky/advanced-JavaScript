b7a4f155f80986ac28e94aad67e7f9ffd66ec24d
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-03 20:04:39 +0600

    adding closer function

---
+++ closure.js
@@ -0,0 +1,23 @@
+function stopeWatch() {
+    let count = 0;
+
+    return function () {
+        count++;
+        // console.log(count);
+        return count;
+    }
+}
+
+const clock1 = stopeWatch();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+
+const clock2 = stopeWatch();
+console.log(clock2());
+console.log(clock2());
+
+console.log(clock1());
+
+console.log(clock2());
+console.log(clock2());
