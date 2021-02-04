d18da4346ebbe32b1c0713c734374c47402b8ec7
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-04 13:25:53 +0600

    fixing ovbject-property

--- object-property.js
+++ object-property.js
@@ -29,21 +29,74 @@
 
 
 
-const student = [
-    { id: 21, name: 'sunny' },
-    { id: 31, name: 'manna' },
-    { id: 12, name: 'moushumi' },
-    { id: 55, name: 'Dipjol' }
-];
+// const student = [
+//     { id: 21, name: 'sunny' },
+//     { id: 31, name: 'manna' },
+//     { id: 12, name: 'moushumi' },
+//     { id: 55, name: 'Dipjol' }
+// ];
 // const output = [];
-
 // for (let i = 0; i < student.length; i++) {
-//     // const names = student[2].name;
-//     const names = student.filter(student = student.name == student[2].name)
-//     output.push(names)
+//     if (i === 2) {
+//         const name = student[i].name;
+//         output.push(name)
+//     }
 // }
 // console.log(output);
 
-const bigger = student.filter(s => s.name == student[2].name)
-console.log(bigger);
+
+
+
+
+
+
+
+
+const student = [
+    { id: 21, name: 'Dipjol' },
+    { id: 31, name: 'manna' },
+    { id: 12, name: 'moushumi' },
+    { id: 55, name: 'sunny' }
+];
+const output = [];
+
+for (let i = 0; i < student.length; i++) {
+    const names = student[1].id;
+    // console.log(names);
+    output.push(names)
+    // if (i === 2) {
+    //     const name = student[i].name;
+    //     output.push(name)
+    // }
+}
+for (let i = 0; i < student.length; i++) {
+    const names = student[2].name;
+    // console.log(names)
+    output.push(names)
+}
+for (let i = 0; i < student.length; i++) {
+    const names = student[3].name;
+    // console.log(names)
+    output.push(names)
+}
+// console.log(output);
+
+function justName() {
+    for (let i = 0; i < output.length; i++) {
+        const element = output[i];
+        console.log(element);
+        return element;
+    }
+
+}
+console.log(justName());
+
+
+
+
+
+
+
+// const bigger = student.filter(s => s.name == student[2].name)
+// console.log(bigger);
 
